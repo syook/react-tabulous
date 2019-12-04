4408392507a651d4e569601004235a20c00267e3
harsh <harsh@localhost.localdomain>
2019-12-04 09:36:55 +0530

    search fix

--- src/containers/search/index.js
+++ src/containers/search/index.js
@@ -14,7 +14,7 @@ export default class SearchProvider extends Component {
 
   componentDidUpdate(prevProps) {
     if (!isEqual(prevProps.data, this.props.data)) {
-      this.setState({ data: [...(this.props.data || [])] });
+      this.search(this.state.searchText);
     }
   }
 
