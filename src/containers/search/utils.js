f030006cb536a9294b4469d4135c549b33c67208
mayanksyook <mayank@syook.com>
2020-10-08 15:20:46 +0530

    fix for issue #41

--- src/containers/search/utils.js
+++ src/containers/search/utils.js
@@ -5,9 +5,20 @@ export const searchObj = (obj, query, searchKeys) => {
     if (Object.prototype.hasOwnProperty.call(searchKeys, key)) {
       const value = obj[key];
       if (typeof value === 'object') {
-        found = searchObj(value, query, searchKeys);
-        if (found) {
-          return found;
+        if (Array.isArray(value)) {
+          let subValue;
+          for (subValue of value) {
+            let result = searchObj(subValue, query, searchKeys);
+            if (result) {
+              found = true;
+              break;
+            }
+          }
+
+          if (found) return found;
+        } else {
+          found = searchObj(value, query, searchKeys);
+          if (found) return found;
         }
       } else if (
         value
@@ -18,9 +29,10 @@ export const searchObj = (obj, query, searchKeys) => {
         found = true;
         return found;
       }
-    }
-  }
+    } // end if key is searchable
+  } // end for every key in object
   return found;
+  // return found;
 };
 
 export const getSearchTextFilteredData = ({ data = [], searchText = '', searchKeys = {} }) => {
