b959969aa21487f0eadaa41d30da267a1435291f
somashekar <somashekar@syook.com>
2021-04-16 17:18:35 +0530

    updated test cases for sort container

--- src/containers/sort/indexFunctionalComponent.test.js
+++ src/containers/sort/indexFunctionalComponent.test.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
+
 import SortProvider from './indexFunctionalComponent';
 
 configure({ adapter: new Adapter() });
+
+const rawData = [
+  { Description: 'Dev', Name: 'David', id: 1, objIndex: 0 },
+  { Description: 'QA', Name: 'John', id: 2, objIndex: 1 },
+];
 
 describe('SortProvider', () => {
   it('should render without crashing', () => {
@@ -14,5 +20,20 @@ describe('SortProvider', () => {
   it('should match snapshot', () => {
     const tree = shallow(<SortProvider />);
     expect(toJson(tree)).toMatchSnapshot();
+  });
+
+  it('should contain ContextProvider', () => {
+    const wrapper = shallow(<SortProvider />);
+    expect(wrapper.find('ContextProvider').exists()).toBe(true);
+  });
+
+  it('ContextProvider should contain prop value rawData as passed prop rawData', () => {
+    const wrapper = shallow(<SortProvider rawData={rawData} />);
+    expect(wrapper.find('ContextProvider').props().value.rawData).toBe(rawData);
+  });
+
+  it('ContextProvider should contain prop value count as passed prop count', () => {
+    const wrapper = shallow(<SortProvider rawData={rawData} count={3} />);
+    expect(wrapper.find('ContextProvider').props().value.count).toBe(3);
   });
 });
