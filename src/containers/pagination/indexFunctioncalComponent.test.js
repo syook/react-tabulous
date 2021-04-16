fe73cb5afb9208b5436de26d0e50a5875015bec9
somashekar <somashekar@syook.com>
2021-04-16 17:15:12 +0530

    test cases for pagination container

--- src/containers/pagination/indexFunctioncalComponent.test.js
+++ src/containers/pagination/indexFunctioncalComponent.test.js
@@ -7,13 +7,53 @@ import PaginationProvider from './indexFunctioncalComponent';
 
 configure({ adapter: new Adapter() });
 
+const data = [
+  { Description: 'Dev', Name: 'David', id: 1, objIndex: 0 },
+  { Description: 'QA', Name: 'John', id: 2, objIndex: 1 },
+];
+
 describe('SortProvider', () => {
   it('should render without crashing', () => {
-    shallow(<PaginationProvider data={[]} />);
+    shallow(<PaginationProvider data={data} />);
   });
 
-  it('should match snapshot', () => {
+  it('should match snapshot if data is not empty', () => {
+    const tree = shallow(<PaginationProvider data={data} />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
+
+  it('should match snapshot if data is empty', () => {
     const tree = shallow(<PaginationProvider data={[]} />);
     expect(toJson(tree)).toMatchSnapshot();
+  });
+
+  it('should contain Table component', () => {
+    const wrapper = shallow(<PaginationProvider data={data} />);
+    expect(wrapper.find('Table').exists()).toBe(true);
+  });
+
+  it('Table should contain sortable as true', () => {
+    const wrapper = shallow(<PaginationProvider data={data} />);
+    expect(wrapper.find('Table').props().sortable).toBe(true);
+  });
+
+  it('Table should contain celled as true', () => {
+    const wrapper = shallow(<PaginationProvider data={data} />);
+    expect(wrapper.find('Table').props().celled).toBe(true);
+  });
+
+  it('Table should contain padded as true', () => {
+    const wrapper = shallow(<PaginationProvider data={data} />);
+    expect(wrapper.find('Table').props().padded).toBe(true);
+  });
+
+  it('should contain Pagination component', () => {
+    const wrapper = shallow(<PaginationProvider data={data} />);
+    expect(wrapper.find('Pagination').exists()).toBe(true);
+  });
+
+  it('Pagination should contain rowCount as passed prop count', () => {
+    const wrapper = shallow(<PaginationProvider data={data} count={2} />);
+    expect(wrapper.find('Pagination').props().rowCount).toBe(2);
   });
 });
