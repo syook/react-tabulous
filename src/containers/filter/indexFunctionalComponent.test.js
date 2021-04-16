5b157bcca3910bc49673d68d72f0d04c3a7b203a
somashekar <somashekar@syook.com>
2021-04-16 17:14:10 +0530

    updated test cases for filter container

--- src/containers/filter/indexFunctionalComponent.test.js
+++ src/containers/filter/indexFunctionalComponent.test.js
@@ -23,4 +23,39 @@ describe('FilterProvider', () => {
     const tree = shallow(<FilterProvider data={data} filterableColumns={[]} />);
     expect(toJson(tree)).toMatchSnapshot();
   });
+
+  it('should contain TableFilter component', () => {
+    const wrapper = shallow(<FilterProvider data={data} filterableColumns={[]} />);
+    expect(wrapper.find('TableFilter').exists()).toBe(true);
+  });
+
+  it('TableFilter should contain disabled as true if data is not empty', () => {
+    const wrapper = shallow(<FilterProvider data={data} filterableColumns={[]} />);
+    expect(wrapper.find('TableFilter').props().disabled).toBe(true);
+  });
+
+  it('TableFilter should contain disabled as true if filterableColumn is not empty', () => {
+    const wrapper = shallow(<FilterProvider data={[]} filterableColumns={data} />);
+    expect(wrapper.find('TableFilter').props().disabled).toBe(true);
+  });
+
+  it('TableFilter should contain disabled as false if data and filterableColumn not empty', () => {
+    const wrapper = shallow(<FilterProvider data={data} filterableColumns={data} />);
+    expect(wrapper.find('TableFilter').props().disabled).toBe(false);
+  });
+
+  it('should contain ContextProvider', () => {
+    const wrapper = shallow(<FilterProvider data={data} filterableColumns={data} />);
+    expect(wrapper.find('ContextProvider').exists()).toBe(true);
+  });
+
+  it('ContextProvider should contain value prop rawdata as passed prop rawData', () => {
+    const wrapper = shallow(<FilterProvider data={data} filterableColumns={data} rawData={data} />);
+    expect(wrapper.find('ContextProvider').props().value.rawData).toEqual(data);
+  });
+
+  it('ContextProvider should contain value prop count as passed prop count', () => {
+    const wrapper = shallow(<FilterProvider data={data} filterableColumns={data} count={3} />);
+    expect(wrapper.find('ContextProvider').props().value.count).toBe(3);
+  });
 });
