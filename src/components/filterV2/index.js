265723084f431951a04612ece772147805c293a3
Savita <savita@syook.com>
2023-02-09 17:24:59 +0530

    fix the issue

--- src/components/filterV2/index.js
+++ src/components/filterV2/index.js
@@ -58,7 +58,7 @@ const FilterV2 = props => {
     const updatedFilters = [...filters];
     updatedFilters.splice(index, 1);
 
-    if (updatedFilters.length > 1) updatedFilters[0].predicate = 'Where';
+    if (updatedFilters.length === 1) updatedFilters[0].predicate = 'Where';
 
     setFilters([...updatedFilters]);
     props.setSelectedFilters(updatedFilters);
@@ -244,7 +244,7 @@ const FilterGrid = props => {
           menuPlacement="auto"
         />
       )}
-      <Icon name="times circle outline" onClick={() => props.removeFilter(props.index)} />
+      <Icon name="times circle outline" onClick={() => props.removeFilter(props.index)} style={{border: '1px solid'}} />
     </div>
   );
 };
