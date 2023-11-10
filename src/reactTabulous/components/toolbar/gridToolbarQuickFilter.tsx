91eba0545524f33762f7ca59e6a87154b335a683
Harshith <harshith@syook.com>
2023-11-10 16:37:58 +0530

    code format

--- src/reactTabulous/components/toolbar/gridToolbarQuickFilter.tsx
+++ src/reactTabulous/components/toolbar/gridToolbarQuickFilter.tsx
@@ -38,7 +38,9 @@ export const GridToolbarQuickFilter: React.FC = () => {
       className="gridToolbarQuickFilter"
       value={searchKey}
       startAdornment={<Icon name="search" size={20} fill="var(--grey-500, #5f6368)" />}
-      endAdornment={searchKey ? <Icon name="close" size={20} fill="var(--grey-500, #5f6368)" onClick={handleClearSearch}/> : null}
+      endAdornment={
+        searchKey ? <Icon name="close" size={20} fill="var(--grey-500, #5f6368)" onClick={handleClearSearch} /> : null
+      }
       onChange={handleChange}
     />
   );
